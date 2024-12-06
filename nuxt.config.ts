82fff7c36430a2d53106fc95459a457d27dcc3f1
MarceloVichar <marcos.marcelo43@gmail.com>
2024-12-06 09:51:19 -0300

    fix g-tag env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,8 +53,7 @@ export default defineNuxtConfig({
   },
 
   gtag: {
-    enabled: process.env.NODE_ENV === 'production',
-    id: process.env.NUXT_GTAG_ID || '',
+    id: process.env.NUXT_G_TAG || '',
   },
 
   i18n: {
